c3f82efb681100ffb2a1881cd8216c6f4f7e4dbf
Subomi <subo@subxmi.com>
2023-01-06 16:24:57 -0500

    main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import loading from './loading.gif'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={loading} className="App-logo" alt="logo" />
+        <div style={{ marginTop: '150px' }}>
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+</div>
+
         <a
           className="App-link"
           href="https://reactjs.org"
